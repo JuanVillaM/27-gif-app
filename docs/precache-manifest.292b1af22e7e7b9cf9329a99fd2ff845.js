758cb78c24062cc7dd0c22a74df0fef47377654d
Juan Villa <juanestebanvillamemdoza@gmail.com>
2021-01-09 16:31:22 -0400

    Others files added

---
+++ docs/precache-manifest.292b1af22e7e7b9cf9329a99fd2ff845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17206494ad9162c74bd4e9161695c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5f6bde348d2f74109e5",
+    "url": "/static/css/main.aff555c8.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f6bde348d2f74109e5",
+    "url": "/static/js/main.f72b587b.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
